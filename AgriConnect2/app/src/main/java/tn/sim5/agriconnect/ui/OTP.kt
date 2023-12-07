80b72646d888e67be701eb0d2682312f7d5a1e1c
MoatazMhamdi <Moataz.mhamdi@esprit.tn>
2023-12-08 00:13:22 +0100

    otp

--- AgriConnect2/app/src/main/java/tn/sim5/agriconnect/ui/OTP.kt
+++ AgriConnect2/app/src/main/java/tn/sim5/agriconnect/ui/OTP.kt
@@ -1,12 +1,22 @@
 package tn.sim5.agriconnect.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import tn.sim5.agriconnect.R
+import tn.sim5.agriconnect.databinding.ActivityEditProfileBinding
+import tn.sim5.agriconnect.databinding.ActivityForgetPasswordBinding
+import tn.sim5.agriconnect.databinding.ActivityOtpBinding
 
 class OTP : AppCompatActivity() {
+    private lateinit var binding: ActivityOtpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_otp)
+        binding = ActivityOtpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnReturn.setOnClickListener {
+            startActivity(Intent(this, ForgetPassword::class.java))
+        }
     }
 }
